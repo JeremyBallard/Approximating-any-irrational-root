f623dd83d759ef96532fd40c8447d0ae6b55002b
JeremyBallard <46586431+JeremyBallard@users.noreply.github.com>
2019-08-13 17:18:56 -0700

    Fixed NaN issue by converting to BigDecimal
    README has all the changes documented

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 Author: Jeremy Ballard
 
-Description: Root is a class meant to implement an algorithm that helps approximate any root you give it, given that the root is irrational. This means that if the user wants to find the root of 4, the program says the root is not irrational and returns the correct root, in double. The program, when run through console, is a simple interface that allows the user to plug in positive values. Unfortunately, I haven't programmed to throw negative x, y, or root approx values yet, but those should be coming very shortly. 
+Version 1.1: Root is a class meant to implement an algorithm that helps approximate any root you give it, given that the root is irrational. This means that if the user wants to find the root of 4, the program says the root is not irrational and returns the correct root, in BigDecimal. The program, when run through console, is a simple interface that allows the user to plug in positive values. Unfortunately, still haven't programmed to throw negative x, y, or root approx values yet, but those should be coming whenever I get around to it. 
 
-For more information on the algorithm itself, I used a variation of Theon of Smyr's approximation for root 2. The approximation is a recursive function that very quickly approximates root 2. I have generalized the approximation for all roots. The only issue is with finding roots past 100, the x and y values, in general, almost always tend towards infinity too quickly. Having x and y values inputted as very small (x, y < 1) is ideal for finding any root above 50. 
+For more information on the algorithm itself, I used a variation of Theon of Smyr's approximation for root 2. The approximation is a recursive function that very quickly approximates root 2. I have generalized the approximation for all roots.
 
-Also an unfortunate side effect is that there are still changes to the number, albeit in the 10^-9 and greater places (but still changes), when the counter hits its 300 max. This 300 iteration max was chosen as a compromise between x and y values still being computable and enough iterations to get nearly even digit shown (in the console version) consistent with what has been computed so far in general mathematics.
+Users can also set the iteration count now. It's reccomended to set the iteration limit to be one or 2 orders of magnitude lower than the irrational root you want to approximate. The currently accurate and tested order of magnitude is 10^5. When chosen root is 10^6, even at 2500 iterations, it is still fairly inaccurate (4th decimal place wrong).
 
-!!!! WARNING !!!!
-This algorithm is HIGHLY UNSTABLE with roots above 100, where x and y are also over 1. Anticipate Infinity over Infinity very frequently, which results in NaN.
+BigDecimal also has the issue of outputting very large numbers into console. This can make output into console confusing and ugly to look at. The approx output is still long but not nearly as bad or incomprehensible as the x and y outputs. 
+
+Changes from previous version:
+-Converted everything double to BigDecimal
+-Added in an user chosen iteration cap
+-Removed recursive function to prefer loop function. Might return back to recursive, but attempted to fix something else and needed loop structure.
+-Added more outputs for the count
+-Finally added the close for scanner
+-Fixed a spelling error for output
--- Root.java
+++ Root.java
@@ -1,64 +1,86 @@
 import java.util.*;
 import java.lang.Math;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Root {
 	//meant to remove dividing by 0, also useless to use algorithm to check for perfect squares
-	public static double approxRoot(double x, double y, int rootFactor){
-		if (y == 0.0){
+	public static BigDecimal approxRoot(BigDecimal x, BigDecimal y, int rootFactor, int userCount){
+		if (y == BigDecimal.valueOf(0.0)){
 			System.out.println("Error - Cannot divide by 0");
-			return -1;
+			return BigDecimal.valueOf(-1.0);
 		}
 		//assumed to round down irrational roots such that when multiplied by itself it will not equal rootFactor
 		if ((int)Math.sqrt(rootFactor) * (int)Math.sqrt(rootFactor) == rootFactor){
 			System.out.println("Has to be an irrational root for this to work");
-			return Math.sqrt(rootFactor);
+			return BigDecimal.valueOf(Math.sqrt(rootFactor));
 		}
-		return approxRoot(x, y, 0, 0.0, rootFactor);
+		return approxRoot(x, y, 0, BigDecimal.valueOf(0.0), rootFactor, userCount);
 	}
-	private static double approxRoot(double x, double y, int count, double approx, int rootFactor){
-		//breaks out of the recusive algorithm, especially since the changes are miniscule at this count
-		if (count == 300){
-			System.out.println("Count @ " + count);
-			System.out.println("x = " + x);
-			System.out.println("y = " + y);
-			return approx;
+	private static BigDecimal approxRoot(BigDecimal x, BigDecimal y, int count, BigDecimal approx, int rootFactor, int userCount){
+		for(int i = 0; i < userCount; i++){
+			//meant to store x and y for new iteration
+			BigDecimal oldX = x;
+			BigDecimal oldY = y;
+			oldX.setScale(100, RoundingMode.HALF_UP);
+			oldY.setScale(100, RoundingMode.HALF_UP);
+			approx = oldX.divide(oldY, 100, RoundingMode.HALF_UP);
+			x = oldX.add(oldY.multiply(BigDecimal.valueOf(rootFactor)));
+			y = oldY.add(oldX);
+			//meant to show user changes through the algorithm
+			switch(count){
+				case 1:
+				case 2:
+				case 5:
+				case 8:
+				case 10:
+				case 15:
+				case 25:
+				case 50:
+				case 100:
+				case 250:
+				case 500:
+				case 1000:
+				case 1500:
+				case 2000:
+				case 2500:
+				case 5000:
+				case 10000:
+					System.out.println("Count @ " + count);
+					System.out.println("x = " + x);
+					System.out.println("y = " + y);
+					System.out.println("approx = " + approx);
+			}
+			count++;
 		}
-		approx = x/y;
-		//meant to show user changes through the algorithm
-		switch(count){
-			case 1:
-			case 2:
-			case 5:
-			case 8:
-			case 10:
-			case 15:
-			case 25:
-			case 50:
-			case 100:
-			case 250:
-				System.out.println("Count @ " + count);
-				System.out.println("x = " + x);
-				System.out.println("y = " + y);
-				System.out.println("approx = " + approx);
-				break;
-			default:
-				break;
-		}
+		System.out.println("Count @ " + count);
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		return approx;
+	}
 
-		return approxRoot(x+rootFactor*y, x+y, count+1, approx, rootFactor);
-	}
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		Root approx = new Root();
-		double root;
+		BigDecimal root = new BigDecimal(0);
+		root.setScale(100, RoundingMode.HALF_UP);
 		System.out.println("This system will approximate any irrational positive root through any x and y");
 		System.out.println("Pick your irrational root");
 		int rootFactor = in.nextInt();
+		System.out.println("Type in how many times you want to iterate this algorithm");
+		int userCount = in.nextInt();
 		System.out.println("Type in an x value");
-		double x = in.nextDouble();
+		BigDecimal x = new BigDecimal(0);
+		x = BigDecimal.valueOf(in.nextDouble());
+		x.setScale(100, RoundingMode.HALF_UP);
 		System.out.println("Type in a y value");
-		double y = in.nextDouble();
-		root = Root.approxRoot(x, y, rootFactor);
-		System.out.println("root "+ rootFactor +" is a approx = " + root);
+		BigDecimal y = new BigDecimal(0);
+		y = BigDecimal.valueOf(in.nextDouble());
+		y.setScale(100, RoundingMode.HALF_UP);
+		root = Root.approxRoot(x, y, rootFactor, userCount);
+		root.setScale(10, RoundingMode.HALF_UP);
+		System.out.println("sqrt "+ rootFactor +" is approx = " + root);
+		//closes the scanner
+		in.close();
 	}
 }
